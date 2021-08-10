fb8c972dc8d7ca455dfeac6481805c432e1063e8
TahhRahh <1234tararyan@gmail.com>
2021-08-10 14:42:54 +0100

    link to github

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ function App() {
         
         <h1>Search for a city </h1>
         <Weather />
+
+        <p>This website is coded by Tara Ryan and is open source:</p>
+        <a href="https://github.com/TahhRahh/react-app-wk4"></a>
         
      
     </div>
