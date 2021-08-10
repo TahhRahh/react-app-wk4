d92f554adb85bb33e9166594b1895bce195fd62c
TahhRahh <1234tararyan@gmail.com>
2021-08-10 14:40:06 +0100

    put app together

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import Weather from "./WeatherSearch";
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+        
+        <h1>Search for a city </h1>
+        <Weather />
+        
+     
+    </div>
+  );
+}
+
+export default App;
